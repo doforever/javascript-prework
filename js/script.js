98f2a4d836c49d5ec5408a4c714f527b390f92d7
Dorota Tomala <doforever@gmail.com>
2020-11-11 17:20:39 +0100

    Add curly braces in js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,130 @@
 'use strict';
+{
+    function getResult(playerMove) {
 
-document.querySelector('#buttons').addEventListener('click', function(event){
-    if (event.target.tagName === "BUTTON"){
-        blowImage();
-        setTimeout(function(){playGame(parseInt(event.target.dataset.choice));}, 1000);
+        let playerWon = false;
+        let computerWon = false;
+        let easyCheck = false;
+        let computerMove = null;
+    
+        function determineWinner () {
+            let randomNumber = getRandomNbr();
+            computerMove = getMoveName(randomNumber);
+            console.log('Komputer wylosował: ' + computerMove);
+            if(( computerMove == 'kamień' && playerMove == 'papier')||( computerMove == 'papier' && playerMove == 'nożyce')||
+                (computerMove == 'nożyce' && playerMove == 'kamień')) {
+                playerWon = true;
+    
+            } else if (computerMove == playerMove) {
+                
+            } else if (gVariant == 'easy' && !easyCheck){
+                easyCheck = true;
+                determineWinner();
+            } else {
+                computerWon = true;
+            }
+        }
+    
+        determineWinner();
+    
+        return {
+            'playerMove': playerMove,
+            'computerMove': computerMove,
+            'playerWon': playerWon,
+            'computerWon': computerWon,
+            'pointsPlayer': result.pointsPlayer,
+            'pointsComputer': result.pointsComputer
+        }
+        
     }
-});
 
-document.querySelector('.menu').addEventListener('click', function(event){
-    chooseVariant(event.target.id);
-});
+    function displayResult() {
+        console.log('Displaying result');
+        console.dir(result);
+    
+        if (result.playerMove) {
+            printMessage('Twój ruch to: ' + result.playerMove);
+            printMessage('Mój ruch to: ' + result.computerMove);
+            
+            if (result.computerWon) {
+                printMessage('Ja wygrywam!');
+            } else if (result.playerWon) {
+                printMessage('Ty wygrywasz!');
+            } else {
+                printMessage('Jest remis :-)');
+            }
+        } else {
+            clearMessages();
+        }
+        document.getElementById('result').innerHTML = result.pointsComputer + ' : ' + result.pointsPlayer;
+    }
+    
+    function playGame(playerInput) {
+        clearMessages();    
+        const playerMove = getMoveName(playerInput);
+        console.log('Gracz wybrał: ' + playerMove);
+        
+        result = getResult(playerMove);
+    
+        if (result.computerWon) {
+            result.pointsComputer++;
+        }
+    
+        if (result.playerWon) {
+            result.pointsPlayer ++;
+        }
+           
+        displayResult(result);
+    }
+    
+    function chooseVariant(buttonId){
+        
+        if (buttonId == 'g-easy'){
+            gVariant = 'easy';
+            console.log('Game variant easy');
+            buttonEasy.classList.add('b-active');
+            buttonStd.classList.remove('b-active');
+        }
+        if (buttonId == 'g-standard'){
+            gVariant = 'standard';
+            console.log('Game variant standard');
+            buttonStd.classList.add('b-active');
+            buttonEasy.classList.remove('b-active');
+        }
+    }
+    
+    function blowImage(){
+        const img = document.querySelector('.picture>img');
+        console.log('Blowing image');
+        img.classList.add('blowImg');
+        setTimeout(function(){img.classList.remove('blowImg');}, 1000);
+    }
 
-document.getElementById('reset').addEventListener('click', function(){
-    result = clearResult();
+    document.querySelector('#buttons').addEventListener('click', function(event){
+        if (event.target.tagName === "BUTTON"){
+            blowImage();
+            setTimeout(function(){playGame(parseInt(event.target.dataset.choice));}, 1000);
+        }
+    });
+
+    document.querySelector('.menu').addEventListener('click', function(event){
+        chooseVariant(event.target.id);
+    });
+
+    document.getElementById('reset').addEventListener('click', function(){
+        result = clearResult();
+        displayResult();
+        console.log('New game');
+    });
+
+
+    const buttonEasy = document.getElementById('g-easy');
+    const buttonStd = document.getElementById('g-standard');
+
+    let gVariant = 'standard';
+    chooseVariant('g-standard');
+    let result = clearResult();
+
+
     displayResult();
-    console.log('New game');
-});
-
-
-const buttonEasy = document.getElementById('g-easy');
-const buttonStd = document.getElementById('g-standard');
-
-let gVariant = 'standard';
-chooseVariant('g-standard');
-let result = clearResult();
-
-
-displayResult();
+}
